86edc9b37189589556101fbde5f009d262d1c324
SeamusWilkins <seamuswilkins0@gmail.com>
2023-09-06 17:19:40 +0100

    Initial commit

---
+++ src/main/java/com/seamus/aibazaar/AibazaarApplication.java
@@ -0,0 +1,13 @@
+package com.seamus.aibazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AibazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AibazaarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seamus/aibazaar/AibazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seamus.aibazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AibazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
